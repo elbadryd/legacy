2347c6d33128e6a4f541b08dfc2154f9ab20ca68
Dario Elbadry <elbadry.d@gmail.com>
2018-11-08 21:11:48 -0600

    chinese done

--- drawit/server/listeners/round/guess.js
+++ drawit/server/listeners/round/guess.js
@@ -25,46 +25,44 @@ module.exports = async ({ data, socket, io }) => {
     const { message, nickname, joinCode } = data;
     const game = await findGameWithJoinCode(joinCode);
 
-    // const translate = new Translate({
-    //   projectId: projId,
-    // });
-    // const text = message;
-    // const target = 'en';
-    // translate.translate(text, target)
-    //   .then((results) => {
-    //     let translations = results[0];
-    //     translations = Array.isArray(translations)
-    //       ? translations
-    //       : [translations];
+    // ping google translate api
+    const translate = new Translate({
+      projectId: projId,
+    });
+    const text = message;
+    const target = 'en';
+    translate.translate(text, target)
+      .then((results) => {
+        let translations = results[0];
+        translations = Array.isArray(translations)
+          ? translations
+          : [translations];
 
-    //     console.log('Translations:');
-    //     translations.forEach((translation, i) => {
-    //       console.log(`${text[i]} => (${target}) ${translation}`);
-    //       translated = translation;
-    //     });
-    //   })
-    //   .catch((err) => {
-    //     console.error('ERROR:', err);
-    //   });
-    //   console.log(game.word);
-    //   console.log(translated);
-    //   console.log(message);
-    // translated.toLowerCase() === game.word || 
-    if (message.toLowerCase() === game.word) {
-      if (!socket.hasGuessedCorrect) {
-        socket.hasGuessedCorrect = true;
-        game.players = game.players.map(player => (
-          player.nickname === nickname
-            ? { nickname, score: player.score + 1 }
-            : player
-        ));
-        await game.save();
+        console.log('Translations:');
+        translations.forEach((translation, i) => {
+          console.log(`${text[i]} => (${target}) ${translation}`);
+          console.log(message, translation, game.word);
+          if (message.toLowerCase() === game.word || translation.toLowerCase() === game.word) {
+            if (!socket.hasGuessedCorrect) {
+              socket.hasGuessedCorrect = true;
+              game.players = game.players.map(player => (
+                player.nickname === nickname
+                  ? { nickname, score: player.score + 1 }
+                  : player
+              ));
+              game.save();
+      
+              io.in(joinCode).emit('round:correct_guess', { nickname, scores: game.players });
+            }
+          } else {
+            io.in(joinCode).emit('round:incorrect_guess', { nickname, message });
+          }
+        });
+      })
+      .catch((err) => {
+        console.error('ERROR:', err);
+      });
 
-        io.in(joinCode).emit('round:correct_guess', { nickname, scores: game.players });
-      }
-    } else {
-      io.in(joinCode).emit('round:incorrect_guess', { nickname, message });
-    }
   } catch (error) {
     socket.emit('round:error', { error });
   }
